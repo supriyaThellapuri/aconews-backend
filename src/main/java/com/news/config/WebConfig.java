1db0aed0766c7c93614d7a8e1eaea1d32835b6d6
supriyaThellapuri <113276487+supriyaThellapuri@users.noreply.github.com>
2024-09-12 00:58:57 +0530

    Update WebConfig.java
    updated

--- src/main/java/com/news/config/WebConfig.java
+++ src/main/java/com/news/config/WebConfig.java
@@ -11,9 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // Adjust as needed
+            .allowedOrigins("https://aconews-app.web.app") 
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
     }
 }
-
